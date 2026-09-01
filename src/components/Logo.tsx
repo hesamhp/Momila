eaaa01b33c2bbaa0dc206031986993425b739e22
hesamhp <=>
2026-09-01 17:18:19 +0330

    adress edit

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -10,7 +10,7 @@ type LogoProps = {
 export default function Logo({ className = "h-10 w-10" }: LogoProps) {
   return (
     <img
-      src="../../public/images/logo.png"
+      src="public\images\logo.png"
       alt="لوگوی مومیلا"
       className={`${className} rounded-[30%] object-cover`}
     />
